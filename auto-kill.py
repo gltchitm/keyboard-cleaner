90ca046b4a9507db10020b7937b08f58da6f75a3
gltchitm <>
2021-03-02 20:23:55 -0500

    change indentation in auto-kill.py

--- auto-kill.py
+++ auto-kill.py
@@ -5,25 +5,25 @@ import sys
 MAX_SECONDS = int(sys.argv[1])
 
 def get_pid():
-  output = str(subprocess.check_output(["bash", "-c", "ps -e | grep KeyboardCleaner"]), "utf-8")
-  return int(output.split("\n")[0].strip().split(" ")[0])
+    output = str(subprocess.check_output(["bash", "-c", "ps -e | grep KeyboardCleaner"]), "utf-8")
+    return int(output.split("\n")[0].strip().split(" ")[0])
 def get_uptime(pid):
-  output = str(subprocess.check_output(["bash", "-c", "ps -p " + str(pid) + " -o etime"]), "utf-8")
-  m, s = output.split("\n")[1].strip().split(":")
-  return int(m), int(s)
+    output = str(subprocess.check_output(["bash", "-c", "ps -p " + str(pid) + " -o etime"]), "utf-8")
+    m, s = output.split("\n")[1].strip().split(":")
+    return int(m), int(s)
 
 def check():
-  try:
-    pid = get_pid()
-    m, s = get_uptime(pid)
-    if m * 60 + s >= MAX_SECONDS:
-      subprocess.Popen(["bash", "-c", "kill -9 " + str(pid)])
-      print("Killed KeyboardCleaner for exceeding the time limit!")
-  except Exception:
-    print("KeyboardCleaner is not open!")
-  else:
-    print("KeyboardCleaner is open but has not reached the time limit...")
-  finally:
-    threading.Timer(2.0, check).start()
+    try:
+        pid = get_pid()
+        m, s = get_uptime(pid)
+        if m * 60 + s >= MAX_SECONDS:
+            subprocess.Popen(["bash", "-c", "kill -9 " + str(pid)])
+        print("Killed KeyboardCleaner for exceeding the time limit!")
+    except Exception:
+        print("KeyboardCleaner is not open!")
+    else:
+        print("KeyboardCleaner is open but has not reached the time limit...")
+    finally:
+        threading.Timer(2.0, check).start()
 
 check()
